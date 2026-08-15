c704a15f3e81dbd925d2e059b17b976fc23d0ceb
Vadym Mazur <vadik.mazur@gmail.com>
2026-08-15 10:37:37 +0300

    implement notes CRUD and search

--- notes/tests.py
+++ notes/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from datetime import date
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+from .models import Category, Note
+
+
+class NoteViewsTests(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(title='Робота')
+        self.note = Note.objects.create(title='Зустріч із командою', text='Текст', reminder=date(2026, 8, 20), category=self.category)
+
+    def test_list_detail_create_update_delete(self):
+        self.assertContains(self.client.get(reverse('note_list')), self.note.title)
+        self.assertContains(self.client.get(reverse('note_detail', args=[self.note.pk])), self.note.text)
+        response = self.client.post(reverse('note_create'), {'title': 'Нова', 'text': 'Текст', 'reminder': '2026-08-21', 'category': self.category.pk})
+        self.assertRedirects(response, reverse('note_list'))
+        response = self.client.post(reverse('note_edit', args=[self.note.pk]), {'title': 'Оновлена', 'text': 'Текст', 'reminder': '2026-08-20', 'category': self.category.pk})
+        self.assertRedirects(response, reverse('note_detail', args=[self.note.pk]))
+        response = self.client.post(reverse('note_delete', args=[self.note.pk]))
+        self.assertRedirects(response, reverse('note_list'))
+
+    def test_search_and_filters(self):
+        response = self.client.get(reverse('note_list'), {'search': 'зустріч ІЗ КОМАНДОЮ', 'category': self.category.pk, 'reminder': '2026-08-20'})
+        self.assertContains(response, self.note.title)
--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import hello_from_notes_app
 
+from .views import NoteCreateView, NoteDeleteView, NoteDetailView, NoteListView, NoteUpdateView
 
 urlpatterns = [
-    path("hello/", hello_from_notes_app),
+    path('', NoteListView.as_view(), name='note_list'),
+    path('create/', NoteCreateView.as_view(), name='note_create'),
+    path('<int:pk>/', NoteDetailView.as_view(), name='note_detail'),
+    path('<int:pk>/edit/', NoteUpdateView.as_view(), name='note_edit'),
+    path('<int:pk>/delete/', NoteDeleteView.as_view(), name='note_delete'),
 ]
